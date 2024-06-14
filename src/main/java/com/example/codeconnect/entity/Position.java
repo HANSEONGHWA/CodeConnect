504c95e3d4250c01cdfa9d03861534619ed114cb
HANSEONGHWA <ghk9206@hanmail.net>
2024-06-14 15:11:14 +0900

    feat: TechStack, Position Enum Class 추가

---
+++ src/main/java/com/example/codeconnect/entity/Position.java
@@ -0,0 +1,5 @@
+package com.example.codeconnect.entity;
+
+public enum Position {
+    백엔드, 프론트엔드, 디자이너, 안드로이드, 데브옵스, IOS, PM, 기획자
+}
---
+++ src/main/java/com/example/codeconnect/entity/TechStack.java
@@ -0,0 +1,6 @@
+package com.example.codeconnect.entity;
+
+public enum TechStack {
+    JAVA, Spring, NodeJs, JavaScript, TypeScript, React, Vue, NextJs, Express, Go, C,
+    Django, Python, Swift, Kotlin, PHP, MySql, MongoDB, GraphQL, NestJs, ReactNative, AWS, Docker, Git, Figma
+}
