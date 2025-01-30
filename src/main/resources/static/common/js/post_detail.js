5f060f491d17aa4bf7f6e509b1fe800bd74da4ca
HANSEONGHWA <ghk9206@hanmail.net>
2025-01-31 02:53:42 +0900

    feat: 상세 페이지 post_detail.js 추가

--- src/main/resources/static/common/js/post_detail.js
+++ src/main/resources/static/common/js/post_detail.js
@@ -3,15 +3,13 @@ $(document).ready(function (){
     const segments = pathname.split('/');
     const postId = segments[2];
     const apiUrl = `/api/posts/${postId}`;
+    const postApiUrl =`/api/answers/${postId}`;
 
     $.ajax( {
         url:apiUrl,
         method:"get",
         success: function (data) {
-            console.log("성공",data);
-            const postTitle = $(".post-title");
-            const postDescription = $(".post-description");
-            postTitle.text(data.title);
+            $(".post-title").text(data.title);
             $(".data-type").text(data.type);
             $(".data-processType").text(data.processType);
             $(".data-size").text(data.size);
@@ -20,10 +18,37 @@ $(document).ready(function (){
             $(".data-period").text(data.period);
             $(".data-position").html(data.position.map(pos => `<span>${pos}</span>`).join(' '));
             $(".data-techStack").html(data.techStack.map(tech => `<span>${tech}</span>`).join(' '));
-            postDescription.text(data.description);
+            $(".post-description").text(data.description);
+            $(".answer-list").html(data.answerList.map(answer => `<li>${answer.comment}</li>`));
+
         },
         error: function (error){
             console.log("에러",error);
         }
+    });
+
+    $(".btn").click(function (){
+        const comment = $(".post-answer").val().trim();
+        if (!comment){
+            alert("댓글을 입력하세요.");
+            return;
+        }
+
+        $.ajax({
+            url: postApiUrl,
+            method: "post",
+            contentType: "application/json",
+            data: JSON.stringify({comment: comment}),
+            success: function (response){
+                if (response){
+                    $('.answer-list').prepend(`<li>${response.comment}</li>`)
+                    $(".post-answer").val((""));
+                }
+            },
+            error:function (error){
+                console.log("response 에러",error);
+            }
+
+        });
     });
 });
